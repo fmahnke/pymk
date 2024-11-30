857aed407a6fcb0a776d5a3acdb5323b7afd7b37
Fritz Mahnke <fritz@mahnke.tech>
2024-11-30 12:25:30 -0700

    add test resource loader

---
+++ test/util.py
@@ -0,0 +1,9 @@
+from mktech.path import Path
+import mktech.resources
+
+
+def resource_path(name: str) -> Path:
+    return mktech.resources.resource_path(
+        'test.data',
+        name
+    ).unwrap()
