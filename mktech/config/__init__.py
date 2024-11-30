50a434d1486245810c8dab8eb4dd1db22fda8f8c
Fritz Mahnke <fritz@mahnke.tech>
2024-11-30 12:56:31 -0700

    schema support for config
    - load defaults from a JSON schema, if provided
    - add load_from_file method. validate loaded data against schema

--- mktech/config/__init__.py
+++ mktech/config/__init__.py
@@ -1,18 +1,34 @@
-from typing import Any
+from typing import Any, Iterator, Optional
+
+import jsonschema
 
 import tomlkit as toml
 from tomlkit import TOMLDocument
 from tomlkit.container import Container
 from tomlkit.items import Item
 
+import mktech.json as json
 from mktech.path import PathInput
+import mktech.toml
+
+from typing import TypeAlias
+
+ValidationError: TypeAlias = jsonschema.exceptions.ValidationError
 
 
 class Config:
     """ Build configuration file. """
 
-    def __init__(self) -> None:
+    def __init__(self, schema: Optional[PathInput] = None) -> None:
         self.config = TOMLDocument()
+
+        if schema is None:
+            self._schema = None
+        else:
+            with open(schema) as file:
+                self._schema = json.load(file)
+
+            self.config.update(json.from_schema(self._schema))
 
     @classmethod
     def from_file(cls, path: PathInput) -> 'Config':
@@ -22,6 +38,11 @@ class Config:
         ctx._init_from_file(path)
 
         return ctx
+
+    def load_from_file(self, path: PathInput) -> None:
+        """ Load the configuration from an existing file. """
+
+        self._init_from_file(path)
 
     def asdict(self) -> dict[str, Any]:
         return dict(self.config)
@@ -39,8 +60,18 @@ class Config:
             fi.write(toml.dumps(self.config))
 
     def _init_from_file(self, path: PathInput) -> None:
-        with open(path, 'r') as fi:
-            self.config = toml.parse(fi.read())
+        with open(path, 'r') as file:
+            toml_document = toml.parse(file.read())
+
+        if self._schema is None:
+            self.config = toml_document
+        else:
+            self.config = mktech.toml.merge(self.config, toml_document)
+
+            jsonschema.validate(self.asdict(), self._schema)
+
+    def __iter__(self) -> Iterator[str]:
+        return self.config.__iter__()
 
     def __getitem__(self, key: str) -> Item | Container:
         return self.config[key]
--- test/config/test_config.py
+++ test/config/test_config.py
@@ -1,29 +1,56 @@
-from importlib import resources
+from mktech.config import Config, ValidationError
 
-from mktech.config import Config
-from mktech.resources import resource_path
+import pytest
 
-_resources = resources.files('test.data')
+from test.util import resource_path
 
 
 class TestConfig:
 
+    def test_missing_key(self) -> None:
+        config = Config.from_file(resource_path('config.toml'))
+
+        assert 'missing_key' not in config
+
     def test_config_to_toml(self) -> None:
         config = Config()
 
-        config['log_level'] = 'DEBUG'
+        config['log_level'] = 'WARNING'
 
-        config['ssh'] = {'use_ssh': True}
+        config['zsh'] = {'fzf': True}
+        config['ssh'] = {'use_ssh': False, 'port': 22}
 
-        result = resource_path('test.data', 'config.toml')
-
-        assert config.toml() == result.unwrap().read_text()
+        assert config.toml() == resource_path('config.toml').read_text()
 
     def test_config_parse_toml(self) -> None:
-        config_toml_path = resource_path('test.data', 'config.toml')
-
-        config = Config.from_file(config_toml_path.unwrap())
+        config = Config.from_file(resource_path('config.toml'))
 
         assert config.asdict() == {
-            'log_level': 'DEBUG', 'ssh': {'use_ssh': True}
+            'log_level': 'WARNING', 'zsh': {'fzf': True},
+            'ssh': {'use_ssh': False, 'port': 22}
+        }
+
+    def test_config_schema_invalid(self) -> None:
+        config = Config(schema=resource_path('config_schema.json'))
+
+        with pytest.raises(ValidationError):
+            config.load_from_file(resource_path('config_invalid.toml'))
+
+    def test_config_schema(self) -> None:
+        config = Config(schema=resource_path('config_schema.json'))
+
+        assert config.asdict() == {
+            'log_level': 'WARNING', 'modules': [],
+            'zsh': {'fzf': False, 'lsd': False}
+        }
+
+    def test_config_schema_update(self) -> None:
+        config = Config(schema=resource_path('config_schema.json'))
+
+        config.load_from_file(resource_path('config.toml'))
+
+        assert config.asdict() == {
+            'log_level': 'WARNING', 'modules': [],
+            'zsh': {'fzf': True,
+                    'lsd': False}, 'ssh': {'use_ssh': False, 'port': 22}
         }
