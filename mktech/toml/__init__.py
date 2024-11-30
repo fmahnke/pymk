e7f05e40d69a94726ac41ab862724845dd622ade
Fritz Mahnke <fritz@mahnke.tech>
2024-11-30 12:32:19 -0700

    add toml package with merge function

---
+++ mktech/toml/__init__.py
@@ -0,0 +1,30 @@
+from tomlkit.items import Table
+from tomlkit import TOMLDocument
+
+from typing import Any
+
+
+def merge(target: TOMLDocument, source: dict[str, Any]) -> TOMLDocument:
+    result = _merge(target, source)
+
+    assert isinstance(result, TOMLDocument)
+
+    return result
+
+
+def _merge(
+    target: TOMLDocument | Table,
+    source: dict[str, Any]
+) -> TOMLDocument | Table:
+    for key, value in source.items():
+        if key not in target:
+            target[key] = value
+        else:
+            target_value = target[key]
+
+            if isinstance(target_value, Table):
+                target[key] = _merge(target_value, value)
+            else:
+                target[key] = value
+
+    return target
---
+++ test/toml/test_toml.py
@@ -0,0 +1,26 @@
+from tomlkit import TOMLDocument
+
+from mktech.toml import merge
+
+
+def test_merge() -> None:
+    document = TOMLDocument()
+
+    document.update(
+        {
+            'log_level': 'WARNING', 'modules': [],
+            'zsh': {'fzf': False,
+                    'lsd': False}, 'ssh': {'use_ssh': False, 'port': 22}
+        }
+    )
+
+    new_doc = merge(
+        document,
+        {'log_level': 'INFO', 'new_key': 'new_val', 'zsh': {'fzf': True}}
+    )
+
+    assert dict(new_doc) == {
+        'log_level': 'INFO', 'new_key': 'new_val', 'modules': [],
+        'zsh': {'fzf': True,
+                'lsd': False}, 'ssh': {'use_ssh': False, 'port': 22}
+    }
