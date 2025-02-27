fe052b5a1c5f26938c1dbbbf13450a7f57bc9fcf
Fritz Mahnke <fritz@mahnke.tech>
2025-02-27 09:58:15 -0700

    error: re-export types from result package

--- mktech/error/__init__.py
+++ mktech/error/__init__.py
@@ -1,2 +1,18 @@
+from typing import TypeAlias, TypeVar
+
+import result
+from result import Err, Ok
+
+T = TypeVar('T')
+
+Result: TypeAlias = result.Result[T, Exception]
+
+__all__ = ['Err', 'Ok', 'Result']
+
+
+class ParseError(Exception):
+    pass
+
+
 def todo(message: str = 'not implemented') -> None:
     raise AssertionError(f'TODO: {message}')
