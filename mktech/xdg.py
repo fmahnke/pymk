1ec66ac6a57cf71aa4ba8bb48261b0b6d5ae26f1
Fritz Mahnke <fritz@mahnke.tech>
2025-03-13 09:16:00 -0700

    add xdg utilities

---
+++ mktech/xdg.py
@@ -0,0 +1,37 @@
+from .os import environ
+from .path import Path
+
+
+def xdg_cache_home() -> Path:
+    return Path(environ('XDG_CACHE_HOME'))
+
+
+def xdg_config_home() -> Path:
+    return Path(
+        environ(
+            'XDG_CONFIG_HOME',
+            str(Path(environ('HOME'), '.config')),
+        )
+    )
+
+
+def xdg_data_home() -> Path:
+    return Path(
+        environ(
+            'XDG_data_HOME',
+            str(Path(environ('HOME'), '.local/share')),
+        )
+    )
+
+
+def xdg_runtime_dir() -> Path:
+    return Path(environ('XDG_RUNTIME_DIR'))
+
+
+def xdg_state_home() -> Path:
+    return Path(
+        environ(
+            'XDG_STATE_HOME',
+            str(Path(environ('HOME'), '.local/state')),
+        )
+    )
