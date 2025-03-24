2e0826ae2052c70f10143fc6ecf04e1e88ea3d81
Fritz Mahnke <fritz@mahnke.tech>
2025-03-24 09:27:02 -0700

    feat(error): reexport more Result API

--- src/mktech/error/__init__.py
+++ src/mktech/error/__init__.py
@@ -1,13 +1,13 @@
 from typing import TypeAlias, TypeVar
 
 import result
-from result import Err, Ok
+from result import Err, Ok, is_err, is_ok
 
 T = TypeVar('T')
 
 Result: TypeAlias = result.Result[T, Exception]
 
-__all__ = ['Err', 'Ok', 'Result']
+__all__ = ['Err', 'Ok', 'Result', 'is_err', 'is_ok']
 
 
 class ParseError(Exception):
