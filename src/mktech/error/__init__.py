a344821a5ddab3ba340e5869d1f498ec9e52a285
Fritz Mahnke <fritz@mahnke.tech>
2025-03-24 10:59:17 -0700

    feat(error): add Exception-like Error type

--- src/mktech/error/__init__.py
+++ src/mktech/error/__init__.py
@@ -1,13 +1,35 @@
-from typing import TypeAlias, TypeVar
+from typing import Any, TypeAlias, TypeVar
 
 import result
 from result import Err, Ok, is_err, is_ok
+
+__all__ = ['Err', 'Error', 'Ok', 'Result', 'is_err', 'is_ok']
 
 T = TypeVar('T')
 
 Result: TypeAlias = result.Result[T, Exception]
 
-__all__ = ['Err', 'Ok', 'Result', 'is_err', 'is_ok']
+
+class Error(Exception):
+    def __init__(
+        self,
+        message: str | None = None,
+        cause: Any = None,
+        *args: Any,
+        **kwargs: Any
+    ) -> None:
+        self.message: str | None = message or self.__class__.__name__
+
+        super().__init__(*args, **kwargs)
+        self.__cause__ = cause
+
+    def __str__(self) -> str:
+        result = f'Error: {self.message}'
+
+        if self.__cause__ is not None:
+            result += f'\nCause: {self.__cause__}'
+
+        return result
 
 
 class ParseError(Exception):
